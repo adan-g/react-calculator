32ec329ae1ad04f79474317b83630de0233ba106
Adan Gimenez <adangimenez@Adans-MBP.attlocal.net>
2023-01-08 12:49:24 -0600

    first commit

---
+++ src/components/Screen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../style-sheets/Screen.css'
+
+const Pantalla = ({ input }) => (
+  <div className='input'>
+    {input}
+  </div>
+)
+
+export default Pantalla;
